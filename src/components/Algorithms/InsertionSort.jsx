1cf747d0dae12624c821a80de89714dfbbf39f18
Abhay Banyal <abhayforservices@gmail.com>
2023-04-09 01:55:49 +0530

    Update animation function and add Insertion Sort algo

--- src/components/Algorithms/InsertionSort.jsx
+++ src/components/Algorithms/InsertionSort.jsx
@@ -1,29 +1,32 @@
-export const doBubbleSort = (array) => {
+export const doInsertionSort = (array) => {
     let animations = [];
     let auxArr = [...array];
-    bubbleSort(animations, auxArr);
+    insertionSort(animations, auxArr);
     return [animations, auxArr];
 }
 
-const bubbleSort = (animations, auxArr) => {
+const insertionSort = (animations, auxArr) => {
     const n = auxArr.length;
-    for(let i = 0; i<n-1; ++i) {
-        let swapped = false;
-        for(let j = 0; j<n-i-1; ++j) {
+    for(let i = 1; i<n; ++i) {
+        let temp = auxArr[i];
+        let j = i-1;
+        while(j >= 0) {
             animations.push({indices:[j, j+1], type: "comp", color: "tomato"});
             animations.push({indices:[j, j+1], type: "comp", color: "#865DFF"});
-            if(auxArr[j] > auxArr[j+1]) {
-                swapped = true;
+            if(auxArr[j] > temp) {
                 animations.push({indices:[j, auxArr[j+1]], type: "swap", color: ""});
                 animations.push({indices:[j+1, auxArr[j]], type: "swap", color: ""});
                 swap(auxArr, j, j+1);
+                j--;
+            }
+            else {
+                break;
             }
         }
-        if(swapped === false) {
-            break;
-        }
+        auxArr[j+1] = temp;
     }
 }
+
 
 const swap = (auxArr, first, second) => {
     [auxArr[first], auxArr[second]] = [auxArr[second], auxArr[first]];
--- src/components/SortingVisualizer/SortingVisualizer.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from "react"
 import "./SortingVisualizer.css"
 import {doBubbleSort} from "../Algorithms/BubbleSort";
-// import {doInsertionSort} from "../Algorithms/InsertionSort";
+import {doInsertionSort} from "../Algorithms/InsertionSort";
 
 const SortingVisualizer = () => {
     const [arr, setArr] = useState([]);
     const [arrSize, setArrSize] = useState(100);
+
     useEffect(() => {
         generateArray(100)
     }, []);
@@ -19,33 +20,44 @@ const SortingVisualizer = () => {
         setArr(a);
     }
 
-    const bubbleSort = () => {
-        let [animations, sortedArr] = doBubbleSort(arr);
+
+    const doAnimations = animations => {
         for(let i = 0; i<animations.length; ++i) {
-            let isColorChange = (i%4 == 0) || (i%4 == 1);
+            let type = animations[i].type;
             let arrBars = document.getElementsByClassName("arr-bar");
-            if(isColorChange) {
-                let color = (i%4 == 0) ? "tomato" : "#865DFF";
-                let [barOneIdx, barTwoIdx] = animations[i];
+            if(type === "comp") {
+                let [barOneIdx, barTwoIdx] = animations[i].indices;
+                let color = animations[i].color;
                 let barOneStyle = arrBars[barOneIdx].style;
                 let barTwoStyle = arrBars[barTwoIdx].style;
                 setTimeout(() => {
                     barOneStyle.backgroundColor = barTwoStyle.backgroundColor = color;
                 },  i*1);
-
             }
             else {
-                let [barIdx, newHeight] = animations[i];
-                if(barIdx == -1) {
-                    continue;
-                }
+                let [barIdx, newHeight] = animations[i].indices;
                 let barStyle = arrBars[barIdx].style;
                 setTimeout(() => {
                     barStyle.height = newHeight + "px";
                 }, i*1); 
             }
         }
-        setArr(sortedArr);
+    }
+
+    const bubbleSort = () => {
+        let [animations, sortedArr] = doBubbleSort(arr);
+        doAnimations(animations);
+        setTimeout(() => {
+            setArr(sortedArr);
+        }, animations.length*1);
+    }
+    
+    const insertionSort = () => {
+        let [animations, sortedArr] = doInsertionSort(arr);
+        doAnimations(animations);
+        setTimeout(() => {
+            setArr(sortedArr);
+        }, animations.length*1);
     }
    
     return <>
@@ -59,8 +71,8 @@ const SortingVisualizer = () => {
         </div>
         <button onClick={() => generateArray(450)}>NEW ARRAY</button>
         <button onClick={bubbleSort}>Bubble Sort</button>
+        <button onClick={insertionSort}>Insertion Sort</button>
         <button onClick={bubbleSort}>Selection Sort</button>
-        <button onClick={bubbleSort}>Insertion Sort</button>
         <button onClick={bubbleSort}>Merge Sort</button>
         <button onClick={bubbleSort}>Quick Sort</button>
         <button onClick={bubbleSort}>Heap Sort</button>
